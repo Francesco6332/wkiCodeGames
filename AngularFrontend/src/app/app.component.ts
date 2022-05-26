5a0a7186489f9fc526d945519ad2e04d0d211463
Gentilozzi Marco <Marco.Gentilozzi@wolterskluwer.com>
2022-05-27 01:21:09 +0200

    Add solution: Angular FE, ASP.Net Core BE

---
+++ AngularFrontend/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public forecasts?: WeatherForecast[];
+  public score?: Number;
+
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+
+    http.get<Number>('/getScore').subscribe(result => {
+      this.score = result;
+    }, error => console.error(error));
+  }
+
+  title = 'AngularFrontend';
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
