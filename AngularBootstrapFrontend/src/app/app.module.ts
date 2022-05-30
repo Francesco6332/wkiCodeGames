c80ca9aefc2d9e7db59802c59e9193558f4a6a4a
Gentilozzi Marco <Marco.Gentilozzi@wolterskluwer.com>
2022-05-30 14:33:50 +0200

    Aggiunto componente header in material

--- AngularBootstrapFrontend/src/app/app.module.ts
+++ AngularBootstrapFrontend/src/app/app.module.ts
@@ -6,13 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatTableModule
+    BrowserModule, HttpClientModule, BrowserAnimationsModule, 
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
