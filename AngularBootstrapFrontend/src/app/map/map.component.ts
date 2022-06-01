4565c2acacaa29706f480e3d47ffec5fa6a8d0b6
Gentilozzi Marco <Marco.Gentilozzi@wolterskluwer.com>
2022-06-01 08:29:38 +0200

    Modificate chiamate REST per lavorare in clud

--- AngularBootstrapFrontend/src/app/map/map.component.ts
+++ AngularBootstrapFrontend/src/app/map/map.component.ts
@@ -1,6 +1,23 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as L from 'leaflet';
+import { icon, Marker } from 'leaflet';
+
+// Codice per forzare l'utilizzo del marker
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+Marker.prototype.options.icon = iconDefault
 
 @Component({
   selector: 'app-map',
@@ -78,5 +95,4 @@ export class MapComponent implements OnInit, AfterViewInit {
   }
   
 }
-
 
