60d6cebde75f2b7fcc0f13ec08a899ac82a3060d
Francesco.Furcillo <francesco.furcillo@wolterskluwer.com>
2022-06-08 16:52:16 +0200

    master: aggiunta oggetti avatar

--- WKGame/WKGameFE/src/app/avatar/avatar.component.ts
+++ WKGame/WKGameFE/src/app/avatar/avatar.component.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvatarComponent implements OnInit {
 
-  constructor() { }
+  public avatar?: Avatar;
+
+  constructor(http: HttpClient) {
+    http.get<Avatar>(`${environment.api_url}/getAvatar/1`).subscribe(result => {
+
+      this.avatar= result;
+
+    }, error => console.error(error));
+   }
 
   ngOnInit(): void {
   }
 
+}
+
+interface Avatar {
+  AvatarId: number;
+  Nickname: string;
+  AvatarImage: string;
+  CurrentScore: number; //Readonly
+  CurrentLevel: number; //Readonly
+  JobRole: string;
+  City: string;
+  CompanySize: number;
+  JobDepartment: string;
+  FollowedCustomers: number;
+  HoursWorkedWeekly: number;
+  HoursRemoteWorkedWeekly: number;
+  Hobbies: string[];
 }
