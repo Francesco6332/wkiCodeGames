9629497c840fa00665d40a325896b04597faff5b
Gentilozzi Marco <Marco.Gentilozzi@wolterskluwer.com>
2022-06-08 02:27:50 +0200

    Aggiunte librerie bootstrap e angular material. Inserite chiamate a backend

--- WKGame/WKGameFrontend/src/app/app.component.ts
+++ WKGame/WKGameFrontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public randomNumber?: Number;
+  public testString?: string;
+
+  constructor(http: HttpClient) {
+    http.get(`${environment.api_url}/getTestString`,  {responseType: 'text'}).subscribe({
+      next: result => { this.testString = result },
+      error: err => { this.testString = "API ERROR" }
+   });
+      
+   http.get<Number>(`${environment.api_url}/getRandomNumber`).subscribe({
+    next: result => { this.randomNumber = result },
+    error: err => { this.randomNumber = -1 }
+ });
+
+  }
+
   title = 'WKGame';
 }
