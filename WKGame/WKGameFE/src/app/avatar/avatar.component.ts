68987bc09848750a3485416d24a886bba4bfcb9f
Francesco Furcillo <francesco.furcillo@wolterskluwer.com>
2022-06-09 10:39:34 +0200

    master: avatar components form lvl1

--- WKGame/WKGameFE/src/app/avatar/avatar.component.ts
+++ WKGame/WKGameFE/src/app/avatar/avatar.component.ts
@@ -21,6 +21,34 @@ export class AvatarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  submitForm() {
+
+    var updateAvatar: Avatar = {
+      Nickname      : this.avatar?.Nickname ?? "",
+      AvatarId      : this.avatar?.AvatarId ?? 0,
+      AvatarImage     : this.avatar?.AvatarImage ?? "",
+      CurrentScore      : this.avatar?.CurrentScore ?? 0,
+      CurrentLevel  : this.avatar?.CurrentLevel ?? 0,
+      JobRole    : this.avatar?.JobRole ?? "",
+      FollowedCustomers   : this.avatar?.FollowedCustomers ?? 0,
+      City     : this.avatar?.City ?? "",
+      CompanySize     : this.avatar?.CompanySize ?? 0,
+      JobDepartment   : this.avatar?.JobDepartment ?? "",
+      HoursWorkedWeekly     : this.avatar?.HoursWorkedWeekly ?? 0,
+      HoursRemoteWorkedWeekly     : this.avatar?.HoursRemoteWorkedWeekly ?? 0,
+      Hobbies     : this.avatar?.Hobbies ?? [],
+  };
+
+    this.http
+      .post(`${environment.api_url}/sendAvatar`, updateAvatar)
+      .subscribe({
+        next: (response) => console.log(response),
+        error: (error) => console.log(error),
+      });
+  }
+
+
+
 }
 
 interface Avatar {
