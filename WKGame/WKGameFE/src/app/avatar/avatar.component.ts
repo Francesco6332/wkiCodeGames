aefd928258fe631fa1a3bf428f5424d073cc3ccf
Gentilozzi Marco <Marco.Gentilozzi@wolterskluwer.com>
2022-06-09 13:10:21 +0200

    Aggiunte automazioni avatar e barra

--- WKGame/WKGameFE/src/app/avatar/avatar.component.ts
+++ WKGame/WKGameFE/src/app/avatar/avatar.component.ts
@@ -10,27 +10,51 @@ import { environment } from 'src/environments/environment';
 export class AvatarComponent implements OnInit {
 
   public avatar?: Avatar;
-  form: FormGroup;
+  public form: FormGroup;
+  public showExtraFields: boolean = false;
+  public imgSrc: string = "../../assets/avatars/Astronauta1.png";
 
   constructor(private http: HttpClient, public fb: FormBuilder) {
-    http.get<Avatar>(`${environment.api_url}/getAvatar/1`).subscribe(result => {
-
-      this.avatar= result;
-
-    }, error => console.error(error));
-
     this.form = this.fb.group({
       clienti_seguiti: [''],
       numero_dipendenti: [''],
       ore_lavorate: [''],
       ore_lavorate_remoto: [''],
       citta: [''],
-      job_role: [''],
+      ruolo: [''],
       hobbies: [''],
       job_department: ['']
     });
 
-   }
+    
+    http.get<Avatar>(`${environment.api_url}/getAvatar/1`).subscribe(result => {
+
+      this.avatar = result;
+
+      this.form = this.fb.group({
+        clienti_seguiti: [this.avatar?.followedCustomers],
+        numero_dipendenti: [this.avatar?.companySize],
+        ore_lavorate: [this.avatar?.hoursWorkedWeekly],
+        ore_lavorate_remoto: [this.avatar?.hoursRemoteWorkedWeekly],
+        citta: [this.avatar?.city],
+        ruolo: [this.avatar?.jobRole],
+        hobbies: [this.avatar?.hobbies],
+        job_department: [this.avatar?.jobDepartment]
+      });
+
+      // Aggiorno i campi dipendenti dal livello
+      this.showExtraFields = this.avatar.currentLevel > 1;
+      this.imgSrc = `../../assets/avatars/Astronauta${this.avatar.currentLevel}.png`;
+
+
+    }, error => {
+      
+      console.error(error);
+    });
+
+    
+
+  }
 
   ngOnInit(): void {
   }
@@ -38,24 +62,24 @@ export class AvatarComponent implements OnInit {
   submitForm() {
 
     var updateAvatar: Avatar = {
-      Nickname      : this.avatar?.Nickname ?? "",
-      AvatarId      : this.avatar?.AvatarId ?? 0,
-      AvatarImage     : this.avatar?.AvatarImage ?? "",
-      CurrentScore      : this.avatar?.CurrentScore ?? 0,
-      CurrentLevel  : this.avatar?.CurrentLevel ?? 0,
-      JobRole    : this.form.get('job_role')?.value ?? "",
-      FollowedCustomers   : this.form.get('clienti_seguiti')?.value ?? 0,
-      City     : this.form.get('citta')?.value ?? "",
-      CompanySize     : this.form.get('numero_dipendenti')?.value ?? 0,
-      JobDepartment   : this.form.get('job_department')?.value ?? "",
-      HoursWorkedWeekly     : this.form.get('ore_lavorate')?.value ?? 0,
-      HoursRemoteWorkedWeekly     : this.form.get('ore_lavorate_remoto')?.value ?? 0,
-      Hobbies     : this.form.get('hobbies')?.value ?? "",
+      nickname: this.avatar?.nickname ?? "",
+      avatarId: this.avatar?.avatarId ?? 0,
+      avatarImage: this.avatar?.avatarImage ?? "",
+      currentScore: this.avatar?.currentScore ?? 0,
+      followedCustomers:        this.form.get('clienti_seguiti')?.value ?? 0,
+      companySize:              this.form.get('numero_dipendenti')?.value ?? 0,
+      hoursWorkedWeekly:        this.form.get('ore_lavorate')?.value ?? 0,
+      hoursRemoteWorkedWeekly:  this.form.get('ore_lavorate_remoto')?.value ?? 0,
+      city:                     this.form.get('citta')?.value ?? "",
+      jobRole:                  this.form.get('ruolo')?.value ?? "",
+      jobDepartment:            this.form.get('job_department')?.value ?? "",
+      hobbies: this.form.get('hobbies')?.value ?? "",
+      currentLevel: this.avatar?.currentLevel ?? 0,
 
-  };
+    };
 
     this.http
-      .post(`${environment.api_url}/setAvatar/${this.avatar?.AvatarId}`, updateAvatar)
+      .post(`${environment.api_url}/setAvatar`, updateAvatar)
       .subscribe({
         next: (response) => console.log(response),
         error: (error) => console.log(error),
@@ -67,17 +91,17 @@ export class AvatarComponent implements OnInit {
 }
 
 interface Avatar {
-  AvatarId: number;    //Readonly
-  Nickname: string;
-  AvatarImage: string;
-  CurrentScore: number; //Readonly
-  CurrentLevel: number; //Readonly
-  JobRole: string;
-  City: string;
-  CompanySize: number;
-  JobDepartment: string;
-  FollowedCustomers: number;
-  HoursWorkedWeekly: number;
-  HoursRemoteWorkedWeekly: number;
-  Hobbies: string[];
+  avatarId: number;    //Readonly
+  nickname: string;
+  avatarImage: string;
+  currentScore: number; //Readonly
+  currentLevel: number; //Readonly
+  jobRole: string;
+  city: string;
+  companySize: number;
+  jobDepartment: string;
+  followedCustomers: number;
+  hoursWorkedWeekly: number;
+  hoursRemoteWorkedWeekly: number;
+  hobbies: string[];
 }
